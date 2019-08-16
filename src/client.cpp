ac797af377ea3d8ac9519708e45c8cdfae829f25
husongtao <husongtao@skybility.com>
2019-08-16 14:30:46 +0800

    add rapidjson

--- src/client.cpp
+++ src/client.cpp
@@ -8,6 +8,33 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+
+using namespace rapidjson;
+
+int test(){
+    // 1. Parse a JSON string into DOM.
+    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    // 3. Stringify the DOM
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+    return 0;
+
+}
 
 void setnonblocking(int fd){
 	int flag = fcntl(fd,F_GETFL,0);
@@ -20,6 +47,9 @@ void setnonblocking(int fd){
 
 int main()
 {
+	test();
+
+
 	const char* ip = "127.0.0.1"; 	
 	int port = 8181;
 
@@ -68,7 +98,7 @@ int main()
 	}
 	for(int i = 0 ;i < nfds; i++){
 		if(events[i].events & EPOLLIN) {
-			fmt::print("Socket {}  connected\n", events[i].data.fd);
+			fmt::print("Socket connected\n");
 		}
 	}
 
